b8327b4408a05d0725ab854e6b4ee0a7b097221d
NaveenaLogu <145955288+NaveenaLogu@users.noreply.github.com>
2024-03-20 14:41:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Formcomponets from './task/formcomponent'
+const App = () => {
+  return (
+    <div>
+     <Formcomponets/>
+    
+    </div>
+  );
+};
+
+export default App;
